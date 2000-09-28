880596e8ed33d125387e410879738f5bc5448626
Simon Horman <horms@verge.net.au>
2000-09-28 23:24:48 +0900

    addest test

---
+++ test/dynamic_array_test.c
@@ -0,0 +1,123 @@
+/**********************************************************************
+ * dynamic_array_test.c                               September 2000
+ * Horms                                             horms@vergenet.net
+ *
+ * vanessa_adt
+ * Library of Abstract Data Types
+ * Copyright (C) 2000  Horms
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ *
+ **********************************************************************/
+
+#include <vanessa_adt.h>
+#include <vanessa_logger.h>
+
+
+/**********************************************************************
+ * Muriel the main function
+ **********************************************************************/
+
+int main (int argc, char **argv){
+  vanessa_logger_t *vl;
+  vanessa_dynamic_array_t *a;
+  char *str;
+  int i;
+
+  /* 
+   * Open logger to filehandle stderr
+   */
+  vl=vanessa_logger_openlog_filehandle(
+    stderr, 
+    "dynamic_array_test", 
+    LOG_DEBUG,
+    0
+  );
+  if(vl==NULL){
+    fprintf(stderr, "Error: vanessa_logger_openlog_filehandle\n");
+    fprintf(stderr, "Fatal Error registering logger. Exiting.\n");
+    exit(-1);
+  }
+
+  /*
+   * Set this as the logger for this programme
+   */
+  vanessa_adt_logger_set(vl);
+
+
+  /*
+   * Create a dynamic array
+   */
+  if((a=vanessa_dynamic_array_create(
+	0, 
+	VANESSA_DESTROY_INT,
+	VANESSA_DUPLICATE_INT,
+	VANESSA_DISPLAY_INT,
+	VANESSA_LENGTH_INT
+   ))==NULL){
+    VANESSA_ADT_LOG(LOG_DEBUG, "main: vanessa_dynamic_array_create");
+    VANESSA_ADT_LOG(LOG_ERR, "Fatal error creating dynamic array. Exiting.");
+    exit(-1);
+  }
+	 
+  /*
+   * Insert some elements
+   */
+  for(i=0;i<8;i++){
+    if((vanessa_dynamic_array_add_element(a, (void *)i))==NULL){
+      VANESSA_ADT_LOG(LOG_ERR, "main: vanessa_dynamic_array_add_element");
+      VANESSA_ADT_LOG(LOG_ERR, "Fatal error creating adding element. Exiting.");
+      exit(-1);
+    }
+  }
+
+  /* 
+   * Display the contents
+   */
+  if((str=vanessa_dynamic_array_display(a, ','))==NULL){
+    VANESSA_ADT_LOG(LOG_DEBUG, "main: vanessa_dynamic_array_display");
+    VANESSA_ADT_LOG(LOG_ERR, "Fatal error displaying dynamic array. Exiting.");
+    exit(-1);
+  }
+  printf("%s\n", str);
+  free(str);
+
+  /*
+   * Reverse the dynamic array
+   */
+  vanessa_dynamic_array_reverse(a);
+
+
+  /* 
+   * Display the contents again
+   */
+  if((str=vanessa_dynamic_array_display(a, ','))==NULL){
+    VANESSA_ADT_LOG(LOG_DEBUG, "main: vanessa_dynamic_array_display 2");
+    VANESSA_ADT_LOG(LOG_ERR, "Fatal error displaying dynamic array. Exiting.");
+    exit(-1);
+  }
+  printf("%s\n", str);
+  free(str);
+
+  /* 
+   * Clean Up
+   */
+  vanessa_dynamic_array_destroy(a);
+  vanessa_adt_logger_unset();
+  vanessa_logger_closelog(vl);
+
+  exit(0);
+}
